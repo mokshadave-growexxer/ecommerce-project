d86291b43b6db6901bb50d0155457fc2d322227c
Moksha Dave <moksha.dave@growexx.com>
2026-02-05 16:52:45 +0530

    Add products API endpoint

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ const app = express();
 app.get('/', (req, res) => {
 res.send('Welcome to the App');
 });
-<<<<<<< HEAD
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
 console.log(`Server running on port ${PORT}`);
-=======
 app.get('/api/users', (req, res) => {
 res.json({ users: [] });
->>>>>>> 6cc8aaa (Add users API endpoint)
+});
+app.get('/api/products', (req, res) => {
+res.json({ products: [] });
 });
 module.exports = app;
