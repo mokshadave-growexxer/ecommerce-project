9331b04a28e299faebfa15ebd552aca464b71d9f
Moksha Dave <moksha.dave@growexx.com>
2026-02-05 15:58:03 +0530

    feat: add password validation helper

--- auth.js
+++ auth.js
@@ -10,10 +10,13 @@ console.log("Logging out");
 }
 function validatePassword(password) {
 return password.length >= 8;
+<<<<<<< HEAD
 >>>>>>> 98b233f (typo fix)
 =======
 }
 function logout() {
 console.log("Logging out");
 >>>>>>> e38b740 (add logout)
+=======
+>>>>>>> 55e7bcd (oops forgot validation)
 }
