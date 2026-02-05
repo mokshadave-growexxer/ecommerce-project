05a74aa8e1a9f68712b028be48bc8e87444d09b6
Moksha Dave <moksha.dave@growexx.com>
2026-02-05 15:56:37 +0530

    fix
    typo fix
    
    add fix

---
+++ auth.js
@@ -0,0 +1,13 @@
+function login(username, password) {
+<<<<<<< HEAD
+// TODO
+=======
+// Validate credentials
+}
+function logout() {
+console.log("Logging out");
+}
+function validatePassword(password) {
+return password.length >= 8;
+>>>>>>> 98b233f (typo fix)
+}
